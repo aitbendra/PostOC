e9333e63ff1dda5c12cdeed96e5aab611cc9e2b0
AIT BENDRA <anouar.ait-bendra@capgemini.com>
2019-03-15 10:19:46 +0100

    first commit

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,27 @@
+import { Component,Input , OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() created_at: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  OnlovePlus(){
+  	this.loveIts++;
+  }
+	OnloveMoins(){
+	  	this.loveIts--;
+	  }
+
+}
